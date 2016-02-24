9e6cae47a25c84dfafbd66655318f9dfc50a1754
vennard <johnlvennard@gmail.com>
2016-02-24 11:12:17 -0700

    added some more basic working structure

--- lib/levelcreation.py
+++ lib/levelcreation.py
@@ -1,6 +1,20 @@
-#!/usr/bin/python2
-
 # Contains methods and resources for level generation
+
+import pygame
+
+WHITE = (255,255,255)
 
 def testcall():
     print("successfully called testcall in lib/levelcreation.py")
+
+class Block(pygame.sprite.Sprite):
+    '''This class represents the basic building blocks of the game level'''
+    def __init__(self, image, pos):
+        # call parent constructor
+        super(Block, self).__init__()
+
+        # initialize image and position
+        self.image = pygame.image.load(image).convert()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
+
--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python2
 
 import pygame
-import lib.levelcreation
+from lib import levelcreation
 
+# test call to all aux scripts
 print("Launching main.py script")
 levelcreation.testcall()
+
+# initialize screen and background
+pygame.init()
+screen = pygame.display.set_mode((1000,1000))
+pygame.display.set_caption('Basic Pygame Program')
+
+# initialize Game Clock
+clk = pygame.time.Clock()
+
+# Test ZONE
+position = [10,10]
+test = levelcreation.Block('images/tile_sample1.png', position)
+testimage = pygame.image.load('images/tile_sample1.png')
+screen.blit(test.image, test.rect)
+pygame.display.flip()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("Exited through canceling screen")
+            exit()
+        elif event.type == pygame.KEYDOWN:
+            print("Pressed KEYDOWN")
+            pass
+        else:
+            pass
+        clk.tick(60) # update x times per second
+        pygame.display.flip()
