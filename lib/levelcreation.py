60aaf49af87afdda64e8c2ed60ef29c47a354e3a
vennard <johnlvennard@gmail.com>
2016-03-03 12:21:20 -0700

    got room generation side wall checks complete

--- lib/levelcreation.py
+++ lib/levelcreation.py
@@ -5,15 +5,13 @@ import random
 
 WHITE = (255,255,255)
 LEVEL_SIZE = 1000
-ROOM_SIZE = 10
+ROOM_SIZE = 20
+BLOCK_SIZE = 10
 
 def testcall():
     print("successfully called testcall in lib/levelcreation.py")
 
 class Block(pygame.sprite.Sprite):
-    blockType = None
-    image = None
-
 
     '''This class represents the basic building blocks of the game level'''
     def __init__(self, image, pos):
@@ -25,14 +23,14 @@ class Block(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
 
-class Room:
-    size = [0,0]    # contains x and y size of room
-    topleft = [0,0]     #contains top left coords of room
-    floor_images = []
-    blocks = []     # initialized empty list for blocks
+    def changeImage(self, newimage):
+        self.image = pygame.image.load(newimage).convert()
 
-    def __init__(self, size, topleft, floor_images):
-        self.size = size
+class Room:
+    #these are class attributes applied to all instances of this class used without self.
+
+    def __init__(self, topleft, floor_images):
+        self.blocks = [] # initialize empty block list
         self.topleft = topleft
         self.floor_images = floor_images 
         # create randomized room
@@ -43,12 +41,10 @@ class Room:
             for y in range(0,rndy):
                 # get random image to create new block and add to blocks
                 rnd_image = random.randint(0,len(floor_images)-1)
-                newx = topleft[0] + 10*x 
-                newy = topleft[1] + 10*y 
+                newx = topleft[0] + BLOCK_SIZE*x 
+                newy = topleft[1] + BLOCK_SIZE*y 
                 self.blocks.append(Block(floor_images[rnd_image],[newx,newy]))
-
-
-
 
     def addBlock(self, block):
         self.blocks.append(block)
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 import pygame
+import random
 from lib import levelcreation
 
 # test call to all aux scripts
@@ -16,25 +17,60 @@ pygame.display.set_caption('Basic Pygame Program')
 clk = pygame.time.Clock()
 
 # TODO testing custom Block class instantiation
+'''
 position = [10,10]
 test = levelcreation.Block('images/tile_sample1.png', position)
 #testimage = pygame.image.load('images/tile_sample1.png')
 screen.blit(test.image, test.rect)
 pygame.display.flip()
+'''
 
 # TODO testing custom room class instantiation
+'''
 pos = [100,100]
 test1 = levelcreation.Block('images/tile1.png', pos)
 size = [1,1]
 floor_images = ['images/tile0.png','images/tile1.png','images/tile2.png']
-test_room = levelcreation.Room(size,pos,floor_images)
+test_room = levelcreation.Room(pos,floor_images)
 #test_room.addBlock(test)
 #test_room.addBlock(test1)
 for i in test_room.blocks:
     screen.blit(i.image, i.rect)
+    '''
 
 #screen.blit(test_room.blocks[*].image, test_room.blocks[*].rect)
 #screen.blit(test_room.blocks[0].image, test_room.blocks[0].rect)
+
+#TODO testing multiple room creation and smart placement
+num_rooms = 100
+#num_rooms = random.randint(1,10)
+floor_images = ['images/tile0.png','images/tile1.png','images/tile2.png','images/tile3.png','images/tile4.png']
+room_list = []
+#if ((npos_e[0] >= cpos_s[0]) and (npos_s[0] <= cpos_e[0])) or ((npos_e[1] >= cpos_s[1]) and (npos_s[1] <= cpos_e[1])):
+#cpos_s = check_room.topleft
+#cpos_e = [cpos_s[0]+check_room.size[0],cpos_s[1]+check_room.size[1]]
+for i in range(0,num_rooms):
+    #Create new room at random coords
+    rndx = random.randint(0,levelcreation.LEVEL_SIZE)
+    rndy = random.randint(0,levelcreation.LEVEL_SIZE)
+    nroom = levelcreation.Room([rndx,rndy],floor_images)
+    npos_s = nroom.topleft
+    npos_e = [npos_s[0]+(nroom.size[0]*levelcreation.BLOCK_SIZE),npos_s[1]+(nroom.size[1]*levelcreation.BLOCK_SIZE)]
+    print "adding room %d at x=%d and y=%d with xend=%d and yend=%d" % (i,rndx,rndy,npos_e[0],npos_e[1])
+    # check for screen size violation
+    if (npos_e[0] >= levelcreation.LEVEL_SIZE) or (npos_e[1] >= levelcreation.LEVEL_SIZE):
+        print "screen size violation on attempt %d" % i 
+        for b in nroom.blocks:
+            b.changeImage('images/tile1.png')
+    else:
+        room_list.append(nroom)
+
+
+# display all rooms in room_list
+for room in room_list:
+    for k in room.blocks:
+        screen.blit(k.image, k.rect)
+
 
 
 while 1:
